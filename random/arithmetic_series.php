b60fee1ff4fa70522c9e132edafd4ef4248f62fb
Kira <kira68784@gmail.com>
2023-08-09 14:08:22 +0700

    Create arithmetic_series.php

---
+++ random/arithmetic_series.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Membuat Form Sederhana</title>
+</head>
+<body>
+    <form method="POST">
+        <div>
+            <label>Nama</label> <br>
+            <input name="n" type="number" placeholder="Masukkan n">
+        </div>
+        <div>
+            <button>Submit</button>
+        </div>
+    </form>
+    <?php
+        function f1($n) {
+            $tot = 0;
+            for ($x = 1; $x <= $n; $x++) {
+            $tot += $x;
+            }
+            return $tot;
+        }
+
+        function f2($n) {
+            return $n/2 * (2 + ($n-1));
+        }
+
+        $n = (int) @$_POST['n'];
+        echo f1($n), " ", f2($n), "\n";
+    ?>
+</body>
+</html>
