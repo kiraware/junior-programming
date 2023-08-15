3c4cfa274c22897c5f9a99ef45b8cb2f8960a521
kiraware <117554978+kiraware@users.noreply.github.com>
2023-08-15 14:39:07 +0700

    belajar upload file

--- public/day6/kodeA/upload.php
+++ public/day6/kodeA/upload.php
@@ -1,17 +1,18 @@
 <?php
-    $target_dir = "uploads/";
-    $target_file = $target_dir . basename($_FILES["berkas"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-    // Check if image file is a actual image or fake image
-    if(isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["berkas"]["tmp_name"]);
-        if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
-        }
+    // ambil data file
+    $namaFile = $_FILES['berkas']['name'];
+    $namaSementara = $_FILES['berkas']['tmp_name'];
+
+    // tentukan lokasi file akan dipindahkan
+    $dirUpload = "uploads/";
+
+    // pindahkan file
+    $terupload = move_uploaded_file($namaSementara, $dirUpload.$namaFile);
+
+    if ($terupload) {
+        echo "Upload berhasil!<br/>";
+        echo "Link: <a href='".$dirUpload.$namaFile."'>".$namaFile."</a>";
+    } else {
+        echo "Upload Gagal!";
     }
 ?>
